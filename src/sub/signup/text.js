991b96660d99371ff520eee1af6abe103a9427ad
chloenam <chloe.namhjoo@gmail.com>
2021-07-14 23:08:08 +0900

    🔨 Update sign up input focus in event, init js file

--- src/sub/signup/text.js
+++ src/sub/signup/text.js
@@ -1,22 +1,38 @@
 $(document).ready(function () {
   let helpArr = [];
-  const help = $('.help_txt p');
+  const help = $('.help_txt > p');
   const input = $('input, textarea');
 
   setArr(help, helpArr);
+  init();
 
   $(input).on('focusin', function () {
     let aClass = $(this).closest('article').attr('class');
-
     let i = $(this).closest('article').index();
+
+    $(help).css({ display: 'block' });
+    $(help).siblings('h2').show();
+    $(help).siblings('.default').hide();
 
     if (aClass == helpArr[i - 1]) {
       $(help).removeClass('on');
       $(help)
         .eq(i - 1)
-        .addClass('on');
+        .addClass('on')
+        .show();
     }
   });
+
+  $('.input_btn > input').on('click', function () {
+    init();
+  });
+
+  function init() {
+    $(help).hide();
+    $(help).siblings('.default').show();
+    $(help).removeClass('on');
+    $(help).siblings('h2').hide();
+  }
 
   function setArr(el, arr) {
     $(el).each(function (index, data) {
