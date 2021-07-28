d19f9b525ccd88d61b5e7fee1401441ad4c332b5
chloenam <chloe.namhjoo@gmail.com>
2021-07-28 17:57:04 +0900

    ✨ Add skip navigation html, css, js

--- src/static/custom.js
+++ src/static/custom.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
+  //scroll
   const section = $('.myScroll');
   const leng = section.length;
-
   let posArr = [];
   let baseLine = -500;
 
+  //skipNavi
+  $('#skipNavi a').on('focusin', function () {
+    $(this).addClass('skip');
+  });
+
+  $('#skipNavi a').on('focusout', function () {
+    $(this).removeClass('skip');
+  });
+
+  //scroll
   setPos();
 
   $(window).on('resize', setPos);
